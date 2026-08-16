612a01c16f0562db71f3c3df9d9bf52e3c298be5
Steve Hommy <steve96hommy@gmail.com>
2026-08-16 21:57:50 +0300

    Exercise 2.5. Documentation and ConfigMaps

--- log_output/README.md
+++ log_output/README.md
@@ -1,15 +1,27 @@
 # Log output
 
-## Exercise 2.3. Keep them separated
+## Exercise 2.5. Documentation and ConfigMaps
 
 ```bash
 kubectl create namespace exercises
 
+cd log_output/writer
+docker build -t ssteevooh/log_output_writer:2.5 .
+docker push ssteevooh/log_output_writer:2.5
+
+cd ../reader
+docker build -t ssteevooh/log_output_reader:2.5 .
+docker push ssteevooh/log_output_reader:2.5
+
+cd ../..
 kubectl apply -f log_output/manifests
 kubectl apply -f ping_pong/manifests
+kubectl delete pod -l app=logoutput -n exercises
 
 kubectl get all -n exercises
+kubectl get configmap -n exercises
 kubectl get ing -n exercises
+kubectl logs deployment/log-output-dep -c log-output-reader -n exercises
 ```
 
 Browser:
--- log_output/reader/reader.js
+++ log_output/reader/reader.js
@@ -1,38 +1,41 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("http")
+const fs = require("fs")
 
-const PORT = process.env.PORT || 3000;
-const LOG_FILE = "/usr/src/app/files/log.txt";
+const PORT = process.env.PORT || 3000
+const LOG_FILE = "/usr/src/app/files/log.txt"
+const INFORMATION_FILE = "/usr/src/app/config/information.txt"
 
 function getPings(callback) {
   http.get("http://ping-pong-svc:2345/pings", (res) => {
-    let data = "";
+    let data = ""
 
     res.on("data", (chunk) => {
-      data += chunk;
-    });
+      data += chunk
+    })
 
     res.on("end", () => {
-      callback(data);
-    });
-  });
+      callback(data)
+    })
+  })
 }
 
 const server = http.createServer((req, res) => {
   if (req.url === "/logoutput") {
     getPings((pings) => {
-      const log = fs.readFileSync(LOG_FILE, "utf8");
+      const log = fs.readFileSync(LOG_FILE, "utf8")
+      const information = fs.readFileSync(INFORMATION_FILE, "utf8").trim()
+      const message = process.env.MESSAGE
 
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(`${log}\nPing / Pongs: ${pings}`);
-    });
-    return;
+      res.writeHead(200, { "Content-Type": "text/plain" })
+      res.end(`file content: ${information}\nenv variable: MESSAGE=${message}\n${log}\nPing / Pongs: ${pings}`)
+    })
+    return
   }
 
-  res.writeHead(404, { "Content-Type": "text/plain" });
-  res.end("404 - Not found");
-});
+  res.writeHead(404, { "Content-Type": "text/plain" })
+  res.end("404 - Not found")
+})
 
 server.listen(PORT, () => {
-  console.log(`Server started in port ${PORT}`);
-});
+  console.log(`Server started in port ${PORT}`)
+})
